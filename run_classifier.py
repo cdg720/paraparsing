85c9b03b524357162a80bac44e8ef6caaae21527
Do Kook Choe <cdg720@gmail.com>
2014-09-25 19:49:05 -0400

    minor change

--- run_classifier.py
+++ run_classifier.py
@@ -1,4 +1,12 @@
+from CoNLL import Corpus
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 
+import sys
+
+def main():
+	if len(sys.argv) != 4:
+		print 'python run_classifier.py gold base dual'
+		sys.exit(0)
+	
